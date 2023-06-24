4ab79362e0833ca5efee159d395c6cbd2b810227
Jerry Hogsett <jhogsett@gmail.com>
2023-06-24 15:03:28 -0700

    worked out reverting inflation split

--- merge_frames.py
+++ merge_frames.py
@@ -97,7 +97,6 @@ class MergeFrames:
         else:
             self.merge_precise(num_width, group_names)
 
-
     def merge_precise(self, num_width, group_names):
         with Mtqdm().open_bar(total=len(group_names), desc="Groups") as group_bar:
             for group_name in group_names:
@@ -133,7 +132,6 @@ class MergeFrames:
                         self.log(f"deleting group {group_path}")
                         shutil.rmtree(group_path)
                     Mtqdm().update_bar(bar)
-
 
     def merge_resynthesis(self, first_index, last_index, num_width, group_names):
         group_size = last_index - first_index
@@ -290,56 +288,86 @@ class MergeFrames:
                 Mtqdm().update_bar(group_bar)
 
 
-
-            # EXPECTED FILES = GROUP SIZE - 1
-
-        # go through each group, renumbering files
-        # FILE COUNT = count of files in group
-
-        # - for each group, the count of files must match EXPECTED FILES
-        #   - except the first group, where the count of files should be EXPECTED FILES - 1
-
-            #   - except the last group, where the count of files should match THAT group size - 1
-                # same group size and expected files calculation just on last group
-
-        # - the renumbering START INDEX is FIRST INDEX + 1
-        # - renumber the files starting START INDEX using NUM WIDTH padding
-
-            # - copy all group files to the output directory
-
-        # - each file should not already exist at destination
-        pass
-
-
     def merge_inflation(self, first_index, last_index, num_width, group_names):
+        group_size = last_index - first_index
         if self.action == "combine":
-            self.merge_inflation_combine(first_index, last_index, num_width)
+            self.merge_inflation_combine(first_index, last_index, num_width, group_size, group_names)
         else:
-            self.merge_inflation_revert(first_index, last_index, num_width)
+            self.merge_inflation_revert(first_index, last_index, num_width, group_size, group_names)
 
-    def merge_inflation_revert(self, first_index, last_index, num_width, group_names):
-            # GROUP SIZE = LAST INDEX - FIRST INDEX
+    def merge_inflation_revert(self, first_index, last_index, num_width, group_size, group_names):
+        expected_files = group_size + 1 # extra file is unneeded ending outer anchor
 
-        # FILE COUNT = count of files in group
+        with Mtqdm().open_bar(total=len(group_names), desc="Groups") as group_bar:
+            for group_index, group_name in enumerate(group_names):
+                group_expected_files = expected_files
 
-        # EXPECTED FILES = GROUP SIZE + 1
-        # - extra file is ending outer anchor frame and won't be copied
-        # go through each group, renumbering files
-        # FILE COUNT = count of files in group
-        # - for each group, the count of files must match EXPECTED FILES
-        #   - except the last group, where the count of files should match THAT group size + 1
-        #   - (GROUP SIZE * (LAST INDEX - FIRST INDEX)) + 1
-              # same group size and expected files calculation just on last group
+                first_index, last_index, _ = self.details_from_group_name(group_name)
 
-            # - the renumbering START INDEX is FIRST INDEX
+                if group_index == len(group_names)-1:
+                    # last group's expected files based on its own name
+                    group_expected_files = last_index - first_index + 1
 
-        # - renumber the files starting START INDEX using NUM WIDTH padding
-        # - copy all group files to the output directory EXCEPT THE LAST FILE
-        #   - except the last group - COPY ALL FILES
-        # - each file should not already exist at destination
-        pass
+                group_files = self.group_files(group_name)
+                if len(group_files) != group_expected_files:
+                    raise RuntimeError(
+                        f"expected {expected_files} files in {group_name} but found {len(group_files)}")
 
-    def merge_inflation_combine(self, first_index, last_index, num_width, group_names):
+                # renumber all present files according to the first, last indexes
+                # including files that will not be ulitmately copied back
+                group_path = self.group_path(group_name)
+                first_index, last_index, _ = self.details_from_group_name(group_name)
+                if self.dry_run:
+                    print(f"[Dry Run] Resequencing files in {group_path}")
+                else:
+                    self.log(f"Resequencing files in {group_path}")
+                    base_filename = "reverted-resynthesis-split"
+
+                    ResequenceFiles(group_path,
+                                    self.file_ext,
+                                    base_filename,
+                                    first_index, 1, 1, 0, num_width,
+                                    True,
+                                    self.log).resequence()
+
+                renamed_group_files = self.group_files(group_name)
+                with Mtqdm().open_bar(total=len(renamed_group_files), desc="Copying") as file_bar:
+                    for file_index, file in enumerate(renamed_group_files):
+                        if group_index < len(group_names)-1:
+                            # for all groups except last, copy all but the last file
+                            # it's anchor frame duplicated during splitting
+                            if file_index == len(renamed_group_files)-1:
+                                Mtqdm().update_bar(file_bar)
+                                continue
+
+                        _, filename, ext = split_filepath(file)
+                        to_filepath = os.path.join(self.output_path, filename + ext)
+                        if os.path.exists(to_filepath):
+                            raise RuntimeError(
+                                f"file {to_filepath} already exists in the output path")
+                        if self.dry_run:
+                            print(f"[Dry Run] copying {file} to {to_filepath}")
+                        else:
+                            self.log(f"copying {file} to {to_filepath}")
+                            shutil.copy(file, to_filepath)
+                        Mtqdm().update_bar(file_bar)
+
+                # restore the original filenames
+                self.log(f"Restoring original filenames in {group_path}")
+                with Mtqdm().open_bar(total=len(renamed_group_files),
+                                        desc="Copying") as bar:
+                    for index, file in enumerate(renamed_group_files):
+                        original_filename = group_files[index]
+                        if self.dry_run:
+                            print(
+                            f"[Dry Run] Restoring file '{file}' to '{original_filename}'")
+                        else:
+                            self.log(f"Restoring file '{file}' to '{original_filename}'")
+                            os.replace(file, original_filename)
+                        Mtqdm().update_bar(bar)
+                Mtqdm().update_bar(group_bar)
+
+    def merge_inflation_combine(self, first_index, last_index, num_width, group_size, group_names):
             # ORIG GROUP SIZE = LAST INDEX - FIRST INDEX
 
         # FILE COUNT = count of files in group
