99c5458fb2dc0309ebe40b031dba748440dfda99
Jerry Hogsett <jhogsett@gmail.com>
2023-07-04 14:15:19 -0700

    add group processing to Clean PNG Files feature

--- guide/simplify_png_files.md
+++ guide/simplify_png_files.md
@@ -9,10 +9,14 @@
 * When using PNG from different workflows, non-matching frames can cause confusion
     * Frames to appear not to match when using _Frame Chooser_
     * Frames have different shapes when using sofware such as _Premiere Pro_
-* PNG files are rewritten inluding only the essential RGB color data
+* PNG files are rewritten including only the essential RGB color data
 
 ## How It Works
-1. Set _PNG files path_ to the path on this server containing the PNG files
-1. Click _Clean_
+1. Choose _Individual Path_ or _Batch Processing_
+    - If **Individual Path**
+        - Set _Input Path_ to a path on this server to the PNG files being cleaned
+    - If **Batch Processing**
+        - Set _Input Path_ to a directory on this server containing PNG frame groups to be cleaned
+1. Click _Clean_ or _Clean Batch_
 1. Each PNG file will be rewritten excluding optional data
-    - Tip: As a bonus, the PNG files usually have a smaller file size
+    - 🎁 As a bonus, the PNG files usually have a smaller file size!
--- tabs/simplify_png_files_ui.py
+++ tabs/simplify_png_files_ui.py
@@ -1,8 +1,11 @@
 """Simplify PNG files feature UI and event handlers"""
+import os
 from typing import Callable
 import gradio as gr
 from webui_utils.simple_config import SimpleConfig
 from webui_utils.simple_icons import SimpleIcons
+from webui_utils.file_utils import get_directories
+from webui_utils.mtqdm import Mtqdm
 from webui_tips import WebuiTips
 from interpolate_engine import InterpolateEngine
 from tabs.tab_base import TabBase
@@ -19,15 +22,37 @@ class SimplifyPngFiles(TabBase):
     def render_tab(self):
         """Render tab into UI"""
         with gr.Tab("Clean PNG Files"):
-            gr.Markdown(SimpleIcons.SPONGE + "Remove Interfering Color Calibration Data ",
+            gr.Markdown(SimpleIcons.SPONGE + "Remove Interfering Non-Essential Data",
                 elem_id="tabheading")
-            input_path_text = gr.Text(max_lines=1, label="PNG Files Path",
-                placeholder="Path on this server to the PNG files to be cleaned")
-            gr.Markdown("*Progress can be tracked in the console*")
-            clean_button = gr.Button("Clean", variant="primary")
+            with gr.Tabs():
+                with gr.Tab(label="Individual Path"):
+                    input_path_text = gr.Text(max_lines=1, label="PNG Files Path",
+                        placeholder="Path on this server to the PNG files to be cleaned")
+                    gr.Markdown("*Progress can be tracked in the console*")
+                    clean_button = gr.Button("Clean", variant="primary")
+                with gr.Tab(label="Batch Processing"):
+                    input_path_batch = gr.Text(max_lines=1, label="PNG File Groups Path",
+                        placeholder="Path on this server to the PNG file groups to be cleaned")
+                    gr.Markdown("*Progress can be tracked in the console*")
+                    clean_batch = gr.Button("Clean Batch", variant="primary")
             with gr.Accordion(SimpleIcons.TIPS_SYMBOL + " Guide", open=False):
                 WebuiTips.simplify_png_files.render()
         clean_button.click(self.clean_png_files, inputs=input_path_text, show_progress=False)
+        clean_batch.click(self.clean_png_batch, inputs=input_path_batch, show_progress=False)
+
+    def clean_png_batch(self, input_path : str):
+        """Clean Batch button handler"""
+        if input_path:
+            self.log(f"beginning batch SimplfyPngFiles processing with input_path={input_path}")
+            group_names = get_directories(input_path)
+            self.log(f"found {len(group_names)} groups to process")
+
+            if group_names:
+                with Mtqdm().open_bar(total=len(group_names), desc="File Group") as bar:
+                    for group_name in group_names:
+                        group_input_path = os.path.join(input_path, group_name)
+                        self.clean_png_files(group_input_path)
+                        Mtqdm().update_bar(bar)
 
     def clean_png_files(self, input_path : str):
         """Clean button handler"""
