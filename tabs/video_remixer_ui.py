188c623ab00c730bc3b78893d7f72545ef4a9b20
Jerry Hogsett <jhogsett@gmail.com>
2023-07-26 21:25:47 -0700

    clear tab 7 error on clicking from tab 6

--- tabs/video_remixer_ui.py
+++ tabs/video_remixer_ui.py
@@ -376,7 +376,8 @@ class VideoRemixer(TabBase):
 
         next_button5.click(self.next_button5,
                     inputs=[resynthesize, inflate, resize, upscale, upscale_option],
-                    outputs=[tabs_video_remixer, message_box5, summary_info6, output_filepath])
+                    outputs=[tabs_video_remixer, message_box5, summary_info6, output_filepath,
+                             message_box61])
 
         back_button5.click(self.back_button5, outputs=tabs_video_remixer)
 
@@ -854,12 +855,13 @@ class VideoRemixer(TabBase):
             return gr.update(selected=6), \
                    gr.update(visible=True), \
                    jot.grab(), \
-                   self.state.output_filepath
+                   self.state.output_filepath, \
+                   None
         else:
             return gr.update(selected=5), \
                    gr.update(
                 value="At least one scene must be set to 'Keep' before processing can proceed"), \
-                   None, None
+                   None, None, None
 
     def back_button5(self):
         return gr.update(selected=4)
