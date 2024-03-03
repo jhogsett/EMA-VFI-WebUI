3c978df25c49b21e21cd5dc75d3a9048e08ba84c
Jerry Hogsett <jhogsett@gmail.com>
2024-03-02 18:33:45 -0800

    fix bug preventing cleansing scenes

--- tabs/video_remixer_ui.py
+++ tabs/video_remixer_ui.py
@@ -2510,7 +2510,12 @@ class VideoRemixer(TabBase):
                                         downsample_scene_path,
                                         content_width,
                                         content_height,
-                                        scale_type)
+                                        int(self.crop_w),
+                                        int(self.crop_h),
+                                        -1,
+                                        -1,
+                                        scale_type,
+                                        crop_type="crop")
                 Mtqdm().update_bar(bar)
 
         self.log("purging scenes before replacing with cleansed scenes")
