d041f433f4487e02cc4ddc02dee94f08ed36544f
Jerry Hogsett <jhogsett@gmail.com>
2025-01-01 06:34:09 -0800

    successfully finding dupe file intersections

--- find_duplicate_files.py
+++ find_duplicate_files.py
@@ -42,6 +42,7 @@ class FindDuplicateFiles:
                 for file in files:
                     file_info = {}
                     file_info["basename"] = os.path.basename(file)
+                    file_info["abspath"] = os.path.abspath(file)
                     stats = os.stat(file)
                     file_info["stats"] = stats
                     file_info["bytes"] = stats.st_size
@@ -61,7 +62,7 @@ class FindDuplicateFiles:
                 # print("\r\n")
 
             result = {}
-            kinds = ["basename", "bytes", "modified", "created", "hash"]
+            kinds = ["abspath", "bytes", "modified", "created", "hash"]
             for kind in kinds:
                 kind_result = []
                 dupes = self.find_duplicate_info(files_info, kind)
@@ -82,34 +83,91 @@ class FindDuplicateFiles:
                     if(kind_result):
                         result[kind] = kind_result
             if result:
-                print()
+                # print()
+                # for kind, dupes in result.items():
+                #     print(f"Duplicates by {kind}")
+
+                #     for dupe in dupes:
+                #         kind_values = sorted(dupe.keys())
+
+                #         for kind_value in kind_values:
+                #             entries = dupe[kind_value]
+                #             if isinstance(kind_value, int):
+                #                 print(f"[{kind_value:,}]")
+                #             elif isinstance(kind_value, float):
+                #                 milliseconds = f"{kind_value - int(kind_value):.4f}"[2:]
+                #                 print(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(kind_value))}.{milliseconds}]")
+                #             else:
+                #                 print(f"[{kind_value}]")
+
+                #             for entry in entries:
+                #                 print(f"{entry['abspath']}")
+                #         print()
+                #     print()
+
+                results = []
                 for kind, dupes in result.items():
-                    print(f"Duplicates by {kind}")
-                    for dupe in dupes:
-                        kind_values = sorted(dupe.keys())
-                        for kind_value in kind_values:
-                            entries = dupe[kind_value]
-                            if isinstance(kind_value, int):
-                                print(f"[{kind_value:,}]")
-                            elif isinstance(kind_value, float):
-                                milliseconds = f"{kind_value - int(kind_value):.4f}"[2:]
-                                print(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(kind_value))}.{milliseconds}]")
-                            else:
-                                print(f"[{kind_value}]")
-                            for entry in entries:
-                                print(f"{entry['basename']}")
-                        print()
+                    for kind_other, dupes_other in result.items():
+                        if kind_other == kind:
+                            continue
+                        for dupe in dupes:
+                            kind_values = sorted(dupe.keys())
+                            for dupe_other in dupes_other:
+                                kind_values_other = sorted(dupe_other.keys())
+                                for kind_value in kind_values:
+                                    entries = dupe[kind_value]
+                                    entries_paths = [entry['abspath'] for entry in entries]
+                                    for kind_value_other in kind_values_other:
+                                        entries_other = dupe_other[kind_value_other]
+                                        entries_paths_other = [entry['abspath'] for entry in entries_other]
+                                        common_entries = list(set(entries_paths).intersection(entries_paths_other))
+                                        if len(common_entries) > 1:
+                                            record = {}
+                                            record["kind1"] = kind
+                                            record["kind2"] = kind_other
+                                            record["kindvalue1"] = kind_value
+                                            record["kindvalue2"] = kind_value_other
+                                            record["dupes"] = common_entries
+                                            results.append(record)
+                                            # print(record)
+                                            # print()
+                for result in results:
+                    print()
+                    print(f"Duplicates by {result['kind1']} and {result['kind2']}:")
+                    for entry in result['dupes']:
+                        print(entry)
                     print()
 
-                    """
-                    for each group of duplicate files within a kind+kind value, check for duplicate files in other kinds, kind values
-                    files that match in other kinds+kind values are more likely to be duplicates of each other
-                    e.g. files can easily have the same filename with different sizes
+                """
+>>> list1 = [1,2,3,4,5,6]
+>>> list2 = [3, 5, 7, 9]
+>>> list(set(list1).intersection(list2))
 
-                    try each kind+kind value against each other
-                      find kind1+kind2 matches
-                      find triple, etc. for more likelihood
-                    """
+                for each group of duplicate files within a kind+kind value, check for duplicate files in other kinds, kind values
+                files that match in other kinds+kind values are more likely to be duplicates of each other
+                e.g. files can easily have the same filename with different sizes
+
+                try each kind+kind value against each other
+                    find kind1+kind2 matches
+                    find triple, etc. for more likelihood
+
+Duplicates by bytes
+[150,601]
+C:\AI\CONDA\EMA-VFI-WebUI\DUPLICATEFILE.py
+C:\AI\CONDA\EMA-VFI-WebUI\video_remixer_processor.py
+
+
+Duplicates by modified
+[2024-10-14 08:58:32.2385]
+C:\AI\CONDA\EMA-VFI-WebUI\DUPLICATEFILE.py
+C:\AI\CONDA\EMA-VFI-WebUI\video_remixer_processor.py
+
+
+Duplicates by hash
+[40c71a4caed30ee7b669785f4ecefd5a3112d5e051317af1ba435a5051fa2c1e]
+C:\AI\CONDA\EMA-VFI-WebUI\DUPLICATEFILE.py
+C:\AI\CONDA\EMA-VFI-WebUI\video_remixer_processor.py
+                """
 
     @staticmethod
     def compute_file_hash(file_path : str, algorithm="sha256") -> str:
