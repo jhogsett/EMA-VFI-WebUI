579cd34b3e46e90c25164e6f7e4110510653d183
Jerry Hogsett <jhogsett@gmail.com>
2025-01-18 10:18:52 -0800

    add batch processing

--- tabs/file_deduplicater_ui.py
+++ tabs/file_deduplicater_ui.py
@@ -21,8 +21,8 @@ class FileDeduplicator(TabBase):
                     log_fn : Callable):
         TabBase.__init__(self, config, engine, log_fn)
 
-    DEFAULT_MESSAGE_FIND = "Click Deduplicate Files to: Scan the path and find matching duplicate files (can take from minutes to hours)"
-    # DEFAULT_MESSAGE_FUNNEL = "Click Assemble Batch to: Combine video clips in batch directories (can take from minutes to hours)"
+    DEFAULT_MESSAGE_SINGLE = "Click Deduplicate Files to: Scan the path and find matching duplicate files (can take from minutes to hours)"
+    DEFAULT_MESSAGE_BATCH = "Click Deduplicate Batch to: Scan the paths in the batch path and find matching duplicate files (can take from minutes to hours)"
 
     def render_tab(self):
         """Render tab into UI"""
@@ -30,31 +30,44 @@ class FileDeduplicator(TabBase):
             gr.Markdown(
                 SimpleIcons.MIRROR + "Extract duplicate files to n separate identical directory structure",
                 elem_id="tabheading")
+
+            with gr.Row():
+                wildcard = gr.Text(value="*.*", max_lines=1, label="Wildcard",
+                    info="Set to a value such as '*.jpg' to choose only 'jpg' files")
+                recursive = gr.Checkbox(value=True, label="Include Sub-Directories",
+                    info="If checked, paths are scanned recursively to find all files")
+            with gr.Row():
+                move_files = gr.Checkbox(value=False, label="Move Duplicates",
+                    info="Leave unchecked to scan files only without making changes")
+                keep_type = gr.Dropdown(value="maxpathcomplex", label="Keep Filter",
+                                        choices=["minpathcomplex", "maxpathcomplex"])
+            with gr.Row():
+                dupe_path = gr.Text(max_lines=1, label="Duplicates Move To Path",
+                    info="(Optional) Path on this server to move the duplciate files & directories to",
+                    placeholder="Leave blank to use default path")
+            with gr.Row():
+                input_path2 = gr.Text(max_lines=1, label="Purge Input Path",
+                    info="(Optional) Path on this server to files & directories as a read-only source of duplicate files",
+                    placeholder="If set, duplicate files are moved only if found in the Purge Path")
+
             with gr.Tabs():
-                with gr.Tab(label="Deduplicate Path"):
+                with gr.Tab(label="Individual Path"):
                     with gr.Row():
                         input_path = gr.Text(max_lines=1, label="Input Path",
                             info="Path on this server to the files & directories to be deduplicated")
-                        input_path2 = gr.Text(max_lines=1, label="Input Path 2",
-                            info="Path on this server to optional files & directories as a read-only source of duplication",
-                            placeholder="If specified, files in Input Path are only moved if duplicates are found in Input Path 2")
-                    with gr.Row():
-                        wildcard = gr.Text(value="*.*", max_lines=1, label="Wildcard",
-                            info="Set to a value such as '*.jpg' to choose only 'jpg' files")
-                        recursive = gr.Checkbox(value=True, label="Include Sub-Directories",
-                            info="If checked, paths are scanned recursively to find all files")
-                    with gr.Row():
-                        move_files = gr.Checkbox(value=False, label="Move Duplicates",
-                            info="Leave unchecked to scan files only without making changes")
-                        keep_type = gr.Dropdown(value="maxpathcomplex", label="Keep Filter",
-                                                choices=["minpathcomplex", "maxpathcomplex"])
-                    dupe_path = gr.Text(max_lines=1, label="Move To Path",
-                        info="Path on this server to move the duplciate files & directories to",
-                        placeholder="Leave blank to use default path")
 
-                    message_box = gr.Markdown(format_markdown(self.DEFAULT_MESSAGE_FIND))
+                    message_box = gr.Markdown(format_markdown(self.DEFAULT_MESSAGE_SINGLE))
                     gr.Markdown("*Progress can be tracked in the console*")
                     deduplicate_button = gr.Button("Deduplicate Files " + SimpleIcons.SLOW_SYMBOL, variant="primary")
+
+                with gr.Tab(label="Batch Processing"):
+                    with gr.Row():
+                        input_path_batch = gr.Text(max_lines=1, label="Batch Input Path",
+                            info="Path on this server to the a directory of files & directories to be deduplicated")
+
+                    message_batch = gr.Markdown(format_markdown(self.DEFAULT_MESSAGE_BATCH))
+                    gr.Markdown("*Progress can be tracked in the console*")
+                    deduplicate_batch = gr.Button("Deduplicate Batch " + SimpleIcons.SLOW_SYMBOL, variant="primary")
 
                 # with gr.Tab(label="Create Duplicate Files Funnel"):
 
@@ -64,53 +77,92 @@ class FileDeduplicator(TabBase):
             inputs=[input_path, input_path2, wildcard, recursive, move_files, keep_type, dupe_path],
             outputs=message_box)
 
-    # def assemble_batch(self, input_path : str):
-    #     """Clean Batch button handler"""
-    #     if not input_path:
-    #         return format_markdown(
-    #             "Enter a path to directories of video clips on this server to get started",
-    #             "warning")
+        deduplicate_batch.click(self.deduplicate_batch,
+            inputs=[input_path_batch, input_path2, wildcard, recursive, move_files, keep_type, dupe_path],
+            outputs=message_batch)
 
-    #     if not os.path.exists(input_path):
-    #         return format_markdown(f"Input path {input_path} was not found", "error")
+    def deduplicate_batch(self,
+                          input_path_batch : str,
+                          input_path2 : str,
+                          wildcard : str,
+                          recursive : bool,
+                          move_files : bool,
+                          keep_type : str,
+                          dupe_path : str):
+        """Deduplicate Files button handler"""
+        if not input_path_batch:
+            return format_markdown(
+                "Enter a path to a directory of files/directories on this server to get started",
+                "warning")
 
-    #     path_names = sorted(get_directories(input_path))
-    #     if not path_names:
-    #         return format_markdown(f"Input path {input_path} does not contain video clip directories", "error")
+        if not os.path.exists(input_path_batch):
+            return format_markdown(f"Input path {input_path_batch} was not found1", "error")
 
-    #     self.log(f"beginning batch combine video clips processing with input_path={input_path}")
-    #     self.log(f"found {len(path_names)} groups to process")
+        if not directory_populated(input_path_batch):
+            return format_markdown(f"Files or directories not found in input path {input_path_batch}",
+                                    "error")
 
-    #     warnings = []
-    #     with Mtqdm().open_bar(total=len(path_names), desc="Assembling Batch") as bar:
-    #         for path_name in path_names:
-    #             clips_input_path = os.path.join(input_path, path_name)
-    #             clips = get_files(clips_input_path)
+        if move_files:
+            if not keep_type:
+                return format_markdown(
+                    "Keep Filter must be specified if Move Duplicates is checked",
+                    "error")
 
-    #             if not clips:
-    #                 message = f"Directory {clips_input_path} is empty - skipping"
-    #                 self.log(message)
-    #                 warnings.append(message)
-    #                 Mtqdm().update_bar(bar)
-    #                 continue
+        if input_path2:
+            if not os.path.exists(input_path2):
+                return format_markdown(f"Input path 2 {input_path2} was not found2", "error")
 
-    #             default_path = input_path
-    #             default_filename = path_name
-    #             first_clip = clips[0]
-    #             _, _, default_ext = split_filepath(first_clip)
-    #             output_filepath = os.path.join(default_path, default_filename + default_ext)
+        if not wildcard:
+            wildcard = "*.*"
 
-    #             try:
-    #                 self.assemble_clips(clips_input_path, output_filepath, interactive=False)
-    #             except ValueError as error:
-    #                 return format_markdown(f"Error: {str(error)}", "error")
+        path_names = sorted(get_directories(input_path_batch))
+        if not path_names:
+            return format_markdown(f"No usable files or directories were found in batch input path {input_path_batch}", "error")
 
-    #             Mtqdm().update_bar(bar)
-    #     if warnings:
-    #         warnings = "\r\n".join(warnings)
-    #         return format_markdown(warnings, "warning")
-    #     else:
-    #         return format_markdown(self.DEFAULT_MESSAGE_BATCH)
+        # self.log(f"beginning batch combine video clips processing with input_path={input_path}")
+        # self.log(f"found {len(path_names)} groups to process")
+
+        path = os.path.normpath(input_path_batch)
+        batch_root_path = path.split(os.sep)[0]
+
+        messages = []
+        with Mtqdm().open_bar(total=len(path_names), desc="Deduplicating Batch") as bar:
+            for path_name in path_names:
+                full_path = os.path.join(input_path_batch, path_name)
+
+                path = os.path.normpath(full_path)
+                parts = path.split(os.sep)
+                root_path = os.path.join(parts[0], os.sep, parts[1])
+                path1_name = full_path[len(root_path)+1:]
+
+                # if dupe_path set, use it as the root instead of
+                # the root of the batch input path
+                if dupe_path:
+                    root_path = dupe_path
+                else:
+                    root_path = batch_root_path
+
+                if input_path2:
+                    path = os.path.normpath(input_path2)
+                    root_path2 = path.split(os.sep)[0]
+                    path2_name = input_path2[len(root_path2)+1:]
+
+                    dupe_path = os.path.join(root_path, f"\DUPES-{path1_name}-{path2_name}")
+                else:
+                    dupe_path = os.path.join(root_path, f"\DUPES-{path1_name}")
+
+                try:
+                    count = self.deduplicate_files(full_path, input_path2, wildcard, recursive, move_files, keep_type, dupe_path, False)
+                    messages.append(f"{count} duplicate files extracted to {dupe_path}")
+                except ValueError as error:
+                    messages.append(f"Skipping path {path_name} due to error: {error}")
+
+                Mtqdm().update_bar(bar)
+        if messages:
+            messages = "\r\n".join(messages)
+            return format_markdown(messages)
+        else:
+            return format_markdown(self.DEFAULT_MESSAGE_BATCH)
 
     def deduplicate_files(self,
                           input_path : str,
@@ -121,6 +173,9 @@ class FileDeduplicator(TabBase):
                           keep_type : str,
                           dupe_path : str,
                           interactive=True):
+        if interactive:
+            1/0
+
         """Deduplicate Files button handler"""
         if not input_path:
             if interactive:
@@ -132,7 +187,7 @@ class FileDeduplicator(TabBase):
 
         if not os.path.exists(input_path):
             if interactive:
-                return format_markdown(f"Input path {input_path} was not found", "error")
+                return format_markdown(f"Input path {input_path} was not found3", "error")
             else:
                 raise ValueError(f"input_path {input_path} not found")
 
@@ -171,9 +226,10 @@ class FileDeduplicator(TabBase):
         if input_path2:
             if not os.path.exists(input_path2):
                 if interactive:
-                    return format_markdown(f"Input path 2 {input_path2} was not found", "error")
+                    1/0
+                    return format_markdown(f"Input path 2 {input_path2} was not found4", "error")
                 else:
-                    raise ValueError(f"input_path {input_path} not found")
+                    raise ValueError(f"input path 2 {input_path2} not found")
 
         if not wildcard:
             wildcard = "*.*"
@@ -197,3 +253,5 @@ class FileDeduplicator(TabBase):
 
         if interactive:
             return format_markdown(f"{count} duplicate files extracted to {dupe_path}")
+        else:
+            return count
